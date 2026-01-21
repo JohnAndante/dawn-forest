82ccae1968fb907a11ee3f5a6e42fc9d945d2134
Walker Silvestre <walker_contas@hotmail.com>
2026-01-20 21:01:26 -0300

    Add jump and gravity mechanics to player
    Introduces a 'jump' input action and implements jump and gravity logic in the player script. Updates player and level scenes to support new movement, including jump speed, gravity, and camera drag settings. Adjusts collision and sprite positions for improved gameplay experience.

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -4,14 +4,37 @@ class_name Player
 @onready var player_sprite: AnimatedSprite2D = $AnimatedTexture
 
 @export var speed: int
+@export var jump_speed: int
+@export var player_gravity: int
 
 var direction: Vector2
+var jump_count: int = 0
+var landing: bool = false
 
-func _physics_process(_delta: float) -> void:
+
+func _physics_process(delta: float) -> void:
 	horizontal_movement_env()
+	vertical_movement_env()
+	
+	gravity(delta)
 	move_and_slide()
 	player_sprite.animate(velocity)
 
 func horizontal_movement_env() -> void:
 	var input_direction: float = Input.get_axis("left", "right")
 	velocity.x = input_direction * speed
+
+func vertical_movement_env() -> void:
+	if is_on_floor():
+		jump_count = 0
+	
+	if Input.is_action_just_pressed("jump") and jump_count < 2:
+		jump_count += 1
+		velocity.y = jump_speed
+
+func gravity(delta: float) -> void:
+	velocity.y += delta * player_gravity
+	
+	if velocity.y >= player_gravity:
+		velocity.y = player_gravity
+	pass
