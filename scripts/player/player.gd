b86a9dd95c2fedbcd9b59974be1458d82951a23b
Walker Silvestre <walker_contas@hotmail.com>
2026-01-21 23:46:29 -0300

    Add attack, defense, and crouch actions to player
    Introduces new input actions for attack, defense, and crouch in the project settings and implements their logic in the player script. Updates the player scene to support these actions and refines the idle animation. This enhances player control and interaction capabilities.

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 class_name Player
 
-@onready var player_sprite: PlayerTexture = $Texture
+#@onready var player_sprite: PlayerTexture = $Texture
+@export var player_sprite: PlayerTexture
 
 @export var speed: int
 @export var jump_speed: int
@@ -9,12 +10,18 @@ class_name Player
 
 var direction: Vector2
 var jump_count: int = 0
-var landing: bool = false
 
+var landing: bool = false
+var attacking: bool = false
+var defending: bool = false
+var crouching: bool = false
+
+var can_track_input: bool = false
 
 func _physics_process(delta: float) -> void:
 	horizontal_movement_env()
 	vertical_movement_env()
+	actions_env()
 	
 	gravity(delta)
 	move_and_slide()
@@ -38,3 +45,30 @@ func gravity(delta: float) -> void:
 	if velocity.y >= player_gravity:
 		velocity.y = player_gravity
 	pass
+
+func actions_env() -> void:
+	attack()
+	crouch()
+	defense()
+
+func attack() -> void:
+	var can_attack: bool = not attacking and not crouching and not defending
+	if Input.is_action_just_pressed("attack") and can_attack and is_on_floor():
+		attacking = true
+
+func crouch() -> void:
+	if Input.is_action_just_pressed("crouch") and is_on_floor() and not defending:
+		crouching = true
+		defending = false
+		can_track_input = false
+	elif not defending:
+		crouching = false
+		can_track_input = true
+
+func defense() -> void:
+	if Input.is_action_pressed("defense") and is_on_floor() and not crouching:
+		defending = true
+		can_track_input = false
+	elif not crouching:
+		defending = false
+		can_track_input = true
