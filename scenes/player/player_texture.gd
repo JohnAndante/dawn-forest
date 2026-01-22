ead749056719b0b7dcc3fee844c7a1a0eed13708
Walker Silvestre <walker_contas@hotmail.com>
2026-01-22 00:19:24 -0300

    Refactor player actions and animations handling
    Reworked player action state logic and animation triggers. Player animation states for attack, defense, crouch, and landing are now more clearly separated and mapped to new animation names. Added flags to PlayerTexture for managing action transitions, and improved input handling in player.gd to prevent conflicting actions.

--- scenes/player/player_texture.gd
+++ scenes/player/player_texture.gd
@@ -4,13 +4,20 @@ class_name PlayerTexture
 @export var animation_path: AnimationPlayer
 @export var player: CharacterBody2D
 
+var suffix: String = "_right"
+var normal_attack: bool = false
+var shield_off: bool = false
+var crouching_off: bool = false
+
 func animate(direction: Vector2) -> void:
 	verify_position(direction)
 
-	if direction.y != 0:
+	if player.attacking or player.defending or player.crouching:
+		action_behaviour()
+	elif direction.y != 0:
 		vertical_behaviour(direction)
 	elif player.landing == true:
-		animation_path.play("land")
+		animation_path.play("landing")
 		player.set_physics_process(false)
 	else:
 		horizontal_behaviour(direction)
@@ -18,8 +25,10 @@ func animate(direction: Vector2) -> void:
 func verify_position(direction: Vector2) -> void:
 	if direction.x > 0:
 		flip_h = false
+		suffix = "_right"
 	elif direction.x < 0:
 		flip_h = true
+		suffix = "_left"
 
 func horizontal_behaviour(direction: Vector2) -> void:
 	if direction.x != 0:
@@ -36,8 +45,24 @@ func vertical_behaviour(direction: Vector2) -> void:
 	elif direction.y < 0:
 		animation_path.play("jump")
 
+func action_behaviour() -> void:
+	if player.attacking and normal_attack:
+		animation_path.play("attack" + suffix)
+	if player.defending and shield_off:
+		animation_path.play("defense")
+		shield_off = false
+	if player.crouching and crouching_off:
+		animation_path.play("crouch")
+		crouching_off = false
+
 func _on_animation_finished(anim_name: StringName) -> void:
 	match anim_name:
-		"land":
+		"landing":
 			player.landing = false
 			player.set_physics_process(true)
+		"attack_left":
+			normal_attack = false
+			player.attacking = false
+		"attack_right":
+			normal_attack = false
+			player.attacking = false
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -22,26 +22,31 @@ func _physics_process(delta: float) -> void:
 	horizontal_movement_env()
 	vertical_movement_env()
 	actions_env()
-	
+
 	gravity(delta)
 	move_and_slide()
 	player_sprite.animate(velocity)
 
 func horizontal_movement_env() -> void:
 	var input_direction: float = Input.get_axis("left", "right")
+	if not can_track_input or attacking:
+		velocity.x = 0
+		return
+
 	velocity.x = input_direction * speed
 
 func vertical_movement_env() -> void:
 	if is_on_floor():
 		jump_count = 0
-	
-	if Input.is_action_just_pressed("jump") and jump_count < 2:
+
+	var can_jump = jump_count < 2 and can_track_input and not attacking
+	if Input.is_action_just_pressed("jump") and can_jump:
 		jump_count += 1
 		velocity.y = jump_speed
 
 func gravity(delta: float) -> void:
 	velocity.y += delta * player_gravity
-	
+
 	if velocity.y >= player_gravity:
 		velocity.y = player_gravity
 	pass
@@ -55,15 +60,17 @@ func attack() -> void:
 	var can_attack: bool = not attacking and not crouching and not defending
 	if Input.is_action_just_pressed("attack") and can_attack and is_on_floor():
 		attacking = true
+		player_sprite.normal_attack = true
 
 func crouch() -> void:
-	if Input.is_action_just_pressed("crouch") and is_on_floor() and not defending:
+	if Input.is_action_pressed("crouch") and is_on_floor() and not defending:
 		crouching = true
 		defending = false
 		can_track_input = false
 	elif not defending:
 		crouching = false
 		can_track_input = true
+		player_sprite.crouching_off = true
 
 func defense() -> void:
 	if Input.is_action_pressed("defense") and is_on_floor() and not crouching:
@@ -72,3 +79,4 @@ func defense() -> void:
 	elif not crouching:
 		defending = false
 		can_track_input = true
+		player_sprite.shield_off = true
